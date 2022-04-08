9c87ee608703d76b1025ee4766274aeabe0833b6
Filip Nilsson <filip.c.nilsson@gmail.com>
2022-04-08 11:57:52 +0200

    cleaner

--- balance_dataset.py
+++ balance_dataset.py
@@ -5,100 +5,72 @@ import random
 train = pd.read_csv('data/train.tsv', '\t')
 not_10 = pd.read_csv('not_dep_summaries/10_word.tsv', '\t')
 not_20 = pd.read_csv('not_dep_summaries/20_word.tsv', '\t')
-not_35 = pd.read_csv('not_dep_summaries/35_word.tsv', '\t')
+not_30 = pd.read_csv('not_dep_summaries/20_word.tsv', '\t')
+not_40 = pd.read_csv('not_dep_summaries/20_word.tsv', '\t')
 not_50 = pd.read_csv('not_dep_summaries/50_word.tsv', '\t')
-not_100 = pd.read_csv('not_dep_summaries/100_word.tsv', '\t')
+#not_100 = pd.read_csv('not_dep_summaries/100_word.tsv', '\t')
 sev_10 = pd.read_csv('severe_summarizes/10_word.tsv', '\t')
-sev_15 = pd.read_csv('severe_summarizes/15_word.tsv', '\t')
+#sev_15 = pd.read_csv('severe_summarizes/15_word.tsv', '\t')
 sev_20 = pd.read_csv('severe_summarizes/20_word.tsv', '\t')
 sev_25 = pd.read_csv('severe_summarizes/25_word.tsv', '\t')
 sev_30 = pd.read_csv('severe_summarizes/30_word.tsv', '\t')
-sev_35 = pd.read_csv('severe_summarizes/35_word.tsv', '\t')
+#sev_35 = pd.read_csv('severe_summarizes/35_word.tsv', '\t')
 sev_40 = pd.read_csv('severe_summarizes/40_word.tsv', '\t')
-sev_45 = pd.read_csv('severe_summarizes/45_word.tsv', '\t')
+#sev_45 = pd.read_csv('severe_summarizes/45_word.tsv', '\t')
 sev_50 = pd.read_csv('severe_summarizes/50_word.tsv', '\t')
 
 original_size = train.Label.value_counts()
 
+for index, row in not_40.sample(5).iterrows():
+    print(index, row)
 
-print(original_size['moderate'], original_size['moderate'] / original_size['moderate'])
-print(original_size['severe'], original_size['moderate'] / original_size['severe'])
-print(original_size['not depression'], original_size['moderate'] / original_size['not depression'])
+print(original_size['moderate'], original_size['moderate'] - original_size['moderate'])
+print(original_size['severe'], original_size['moderate'] - original_size['severe'])
+print(original_size['not depression'], original_size['moderate'] - original_size['not depression'])
+
+severe_per = round((original_size['moderate'] - original_size['severe']) / 6) # 6 Because we have 6 summarized datasets
+not_per = round((original_size['moderate'] - original_size['not depression']) / 5) # 6 Because we have 6 summarized datasets
+
+print("balancing severe per class : ", severe_per)
+print("balancing not depression per class : ", not_per)
+
 count = 0
 
 random.seed(42)
 
-#pids = random.sample(train['PID'].tolist(), int(len(train['PID']) * 0.25))
-#print(len(pids))
-
-with open('75_balanced_dataset.tsv', 'wt') as out_file:
+with open('50_balanced_dataset.tsv', 'wt') as out_file:
     tsv_writer = csv.writer(out_file, delimiter='\t')
     tsv_writer.writerow(['PID', 'Text_data', 'Label'])
 
     for index, row in train.iterrows():
         tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-        count += 1
 
-    severe = original_size['severe']
-    not_dep = original_size['not depression']
-    compare = original_size['moderate'] // 2 + original_size['moderate'] // 4
-    print(severe, not_dep)
+    # Severe depression balancing
+    for index, row in sev_10.sample(severe_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
+    for index, row in sev_20.sample(severe_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
+    for index, row in sev_25.sample(severe_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
+    for index, row in sev_30.sample(severe_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
+    for index, row in sev_40.sample(severe_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
+    for index, row in sev_50.sample(severe_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
 
-    for index, row in sev_10.iterrows():
-        if severe < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            severe += 1
-    for index, row in sev_15.iterrows():
-        if severe < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            severe += 1
-    for index, row in sev_20.iterrows():
-        if severe < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            severe += 1
-    for index, row in sev_25.iterrows():
-        if severe < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            severe += 1
-    for index, row in sev_30.iterrows():
-        if severe < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            severe += 1
-    for index, row in sev_35.iterrows():
-        if severe < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            severe += 1
-    for index, row in sev_40.iterrows():
-        if severe < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            severe += 1
-    for index, row in sev_45.iterrows():
-        if severe < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            severe += 1
-    
-    for index, row in not_10.iterrows():
-        if not_dep < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            not_dep += 1
-    for index, row in not_20.iterrows():
-        if not_dep < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            not_dep += 1
-    for index, row in not_35.iterrows():
-        if not_dep < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            not_dep += 1
-    for index, row in not_50.iterrows():
-        if not_dep < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            not_dep += 1
-    for index, row in not_100.iterrows():
-        if not_dep < compare:
-            tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
-            not_dep += 1
+    # Not depression balancing
+    for index, row in not_10.sample(not_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
+    for index, row in not_20.sample(not_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
+    for index, row in not_30.sample(not_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
+    for index, row in not_40.sample(not_per).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
+    for index, row in not_50.sample(not_per - 2).iterrows():
+        tsv_writer.writerow([row['PID'], row['Text_data'], row['Label']])
 
 
-print(severe, not_dep)
-new = pd.read_csv('75_balanced_dataset.tsv', '\t')
+new = pd.read_csv('50_balanced_dataset.tsv', '\t')
 print("New:", new.Label.value_counts())
